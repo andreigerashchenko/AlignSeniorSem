8fdd192739aaa1c398246bb14cd874528a128f0c
andreigerashchenko <67013056+andreigerashchenko@users.noreply.github.com>
2023-04-19 13:16:27 -0400

    First pass at automated horizon detection

--- detect.py
+++ detect.py
@@ -1,5 +1,6 @@
 import cv2
 import numpy as np
+from scipy.interpolate import splprep, splev
 
 # Load the image
 img = cv2.imread('examples\R0011132.JPG')
@@ -12,41 +13,37 @@ if img.shape[0] > 720 or img.shape[1] > 1280:
 # Convert to grayscale
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
+# Apply thresholding
+ret, thresh = cv2.threshold(gray, 127, 255, cv2.THRESH_BINARY)
+
 # Apply canny edge detection
-edges = cv2.Canny(gray, 50, 150, apertureSize=3)
+edges = cv2.Canny(thresh, 50, 150, apertureSize=3)
 
 # Find contours
-contours, hierarchy = cv2.findContours(edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+contours, hierarchy = cv2.findContours(edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
 
-# Find the contour with the longest length that approximates a sine wave
+# Find the contour with the longest length that can be represented by a spline
 max_length = 0
 horizon_contour = None
 for cnt in contours:
-    # Approximate the contour with a polygon
-    epsilon = 0.1 * cv2.arcLength(cnt, True)
-    approx = cv2.approxPolyDP(cnt, epsilon, True)
+    # Check if the contour has enough points to be represented by a spline
+    if len(cnt) >= 50:
+        # Fit a spline to the contour
+        x, y = cnt.squeeze().T
+        tck, u = splprep([x, y], s=0, k=min(3, len(x)-1))
 
-    # Check if the polygon has enough points to approximate a sine wave
-    if len(approx) >= 20:
-        # Fit a sine wave to the polygon
-        x, y = approx.squeeze().T
-        params = cv2.fitLine(approx, cv2.DIST_L2, 0, 0.01, 0.01)
-        vx, vy, x0, y0 = params
-        a = vy / vx
-        b = y0 - a * x0
-        sine_y = a * x + b
+        # Compute the length of the spline
+        length = 0
+        for i in range(len(x)-1):
+            p1 = splev(u[i], tck)
+            p2 = splev(u[i+1], tck)
+            length += np.linalg.norm(np.array(p2)-np.array(p1))
 
-        # Compute the mean squared error between the polygon and the sine wave
-        mse = np.mean((y - sine_y) ** 2)
-
-        # Check if the mean squared error is below a threshold
-        if mse < 100:
-            # Compute the length of the contour
-            length = cv2.arcLength(cnt, closed=True)
-
-            # Check if the contour is the longest so far
-            if length > max_length:
-                max_length = length
+        # Check if the spline is the longest so far and doesn't cross over the same x coordinate more than once
+        if length > max_length:
+            max_length = length
+            spline_x = splev(np.linspace(0, 1, 1000), tck)[0]
+            if len(spline_x) == len(set(np.round(spline_x, 2))):
                 horizon_contour = cnt
 
 # Draw the contour on the image
